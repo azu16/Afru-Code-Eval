fb09d8e2bb4f7b44980ec6059bffc59c00b079fc
azu16 <azamali16@gmail.com>
2017-02-26 16:34:05 -0800

    Update ArrangeString.java
    Check out the comments.

--- Prog10/ArrangeString.java
+++ Prog10/ArrangeString.java
@@ -17,6 +17,22 @@ public class ArrangeString
 		  }
 		  return fin;
    }
+/*Afru comments: are you aware of "StringBuilder" class - can you try to use it here? Can you tell me 
+   why using StringBuilder will be better than string concatenation
+*/	
+ public String rev(String s1)
+ {
+     char[] a = s1.toCharArray();
+     char temp = a[0];
+     a[0] = a[a.length - 1];
+     a[a.length - 1] = temp;
+     //For Loop must omit first and last element	 
+     for(int i = 1; i < a.length-1; i++)
+	      {
+			  fin = fin + a[i];
+		  }
+		  return fin;
+   }
 
 
  }
