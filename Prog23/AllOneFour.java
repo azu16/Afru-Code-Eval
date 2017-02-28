bffcc5a2abc76935d4fbe9413771ce734b07b5d5
azu16 <azamali16@gmail.com>
2017-02-27 16:39:51 -0800

    Take a look at my implementation - test it.

--- Prog23/AllOneFour.java
+++ Prog23/AllOneFour.java
@@ -17,4 +17,17 @@ public class AllOneFour
 	    b1 = true;
 	    return b1;
   }
+  	
+  //only14({1, 4, 2, 4}) -> false. In this example as soon as we find 2 we must return false. Using the same logic, instead of 
+  //looking for every element as 1 or 4 and counting - we can return false as soon as we see any number other than 1 or 4	
+  public boolean afruFind(int[] a1)
+  {
+	  for(int i = 0; i < a1.length; i++)
+	  {
+	      if(a1[i] !=1 && a1[i] != 4){
+	          return false;
+	      }
+	  }
+	  return true;
+  }
 }
