9c146db60cafc21adb6f04b5daee58a8a0693cea
azu16 <azamali16@gmail.com>
2017-03-01 14:59:40 -0800

    Update EndWith.java
    Look at my comments.

--- Prog31/EndWith.java
+++ Prog31/EndWith.java
@@ -2,6 +2,11 @@
 Sample Input: 1. endsLy("oddly") -> true , 2. endsLy("y") -> false ,
 Sample Output: 3. endsLy("oddy") -> false
 */
+
+
+
+//Afru CCOMMENTS : What if the string is 'null'? if its null them this code will generate a NULL pointer exception
+// DO you know what this exception means and which line of the code is it generated at?
 public class EndWith
 {
 	boolean b1;
