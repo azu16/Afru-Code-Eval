ca182f734c497b1746d2ff8948445657377422e4
azu16 <azamali16@gmail.com>
2017-03-03 14:07:50 -0800

    Update TwoNextTwo.java
    Another approach to implement - simply read through don waste too much time on it.

--- Prog32/TwoNextTwo.java
+++ Prog32/TwoNextTwo.java
@@ -33,4 +33,23 @@ public class TwoNextTwo
 	     return b1;
 
   }
+  // AFRU COMMENTS : this is another approach to implement the solution, nothing wrong with your approach - this function
+ // catches the wrong pattern quicker instead of waiting untill the end.
+  public boolean anotherApproach(int[] input){
+      if(input==null){
+          return false;
+      }    
+      boolean hasTwos = false,hasFour=false;
+      for(int i=0;i<input.length-1;i++){    
+	  if(hasTwos==true && hasFours==true){
+	      return false;
+	  }    
+          if(hasTwos==false && input[i]==2 && input[i+1]==2){
+		  hasTwos = true;
+	  }
+	  else if(hasFours==false && input[i]==4 && input[i+1]==4){
+	      hasFours = true;
+	  }    
+      }	  
+  }	
 }
